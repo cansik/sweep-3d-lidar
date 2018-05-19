65705feb27ef1399c21abb7550072d5c40d59018
Florian Bruggisser <florian@nexpose.ch>
2018-05-20 01:09:27 +0200

    added animations for project

---
+++ test/pcx/Assets/Scripts/Orbit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+	public GameObject Target;
+	public float SpeedMod = 10.0f; 
+	private Vector3 _point;
+ 
+	void Start () {
+		_point = Target.transform.position;
+		transform.LookAt(_point);
+	}
+ 
+	void Update () {
+		transform.RotateAround (Target.transform.position, Vector3.up, SpeedMod * Time.deltaTime);  
+	}
+}
